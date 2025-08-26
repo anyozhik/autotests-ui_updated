e8018e317d0203ad6d53f8c1cce4552e5044bd63
anyozhik <ateslyuk5@gmail.com>
2025-08-26 14:25:37 +0700

    Adding test cases

---
+++ tests/dashboard/test_dashboard.py
@@ -0,0 +1,35 @@
+from config import settings
+from pages.dashboard.dashboard_page import DashboardPage
+import pytest
+import allure
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from allure_commons.types import  Severity
+
+from tools.routes import AppRoute
+
+
+@pytest.mark.regression
+@pytest.mark.dashboard
+@allure.tag(AllureTag.REGRESSION, AllureTag.DASHBOARD)
+@allure.epic(AllureEpic.LMS)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.feature(AllureFeature.DASHBOARD)
+@allure.suite(AllureFeature.DASHBOARD)
+@allure.story(AllureStory.DASHBOARD)
+@allure.sub_suite(AllureStory.DASHBOARD)
+class TestDashboard:
+    @allure.title('Check displaying of dashboard page')
+    @allure.severity(Severity.NORMAL)
+    def test_dashboard_displaying(self, dashboard_page_with_state: DashboardPage):
+        dashboard_page_with_state.visit(AppRoute.DASHBOARD)
+
+        dashboard_page_with_state.navbar.check_visible(settings.test_user.username)
+        dashboard_page_with_state.sidebar.check_visible()
+        dashboard_page_with_state.dashboard_toolbar_view.check_visible()
+        dashboard_page_with_state.students_chart_view.check_visible('Students')
+        dashboard_page_with_state.activities_chart_view.check_visible('Activities')
+        dashboard_page_with_state.courses_chart_view.check_visible('Courses')
+        dashboard_page_with_state.scores_chart_view.check_visible('Scores')
