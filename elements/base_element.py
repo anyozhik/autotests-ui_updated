d3dc273fa6b094f5a79240eed3d97b517f5b1100
anyozhik <ateslyuk5@gmail.com>
2025-09-10 23:03:57 +0700

    UI-coverage practice

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Page, Locator, expect
 import allure
+from elements.ui_coverage import tracker
+from ui_coverage_tool import ActionType, SelectorType
 
 from tools.logger import get_logger
 
@@ -18,12 +20,26 @@ class BaseElement:
             logger.info(step)
             return self.page.get_by_test_id(locator).nth(nth)
 
+    def get_row_locator(self, nth: int = 0, **kwargs) -> str:
+        return f"//*[@data-testid='{self.locator.format(**kwargs)}'][{nth + 1}]"
+
+    def track_coverage(self, action_type: ActionType, nth: int = 0, **kwargs):
+        tracker.track_coverage(
+            selector=self.get_row_locator(nth, **kwargs),
+            action_type=action_type,
+            selector_type=SelectorType.XPATH
+        )
+
+
     def click(self, nth: int = 0, **kwargs):
         step = f'Clicking {self.type_of} "{self.name}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.click()
+
+        self.track_coverage(ActionType.CLICK, nth, **kwargs)
+
 
     def check_visible(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is visible'
@@ -32,12 +48,16 @@ class BaseElement:
             logger.info(step)
             expect(locator).to_be_visible()
 
+        self.track_coverage(ActionType.VISIBLE, nth, **kwargs)
+
     def check_text(self, text: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has text "{text}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_text(text)
+
+        self.track_coverage(ActionType.TEXT, nth, **kwargs)
 
     @property
     def type_of(self) -> str:
--- elements/input.py
+++ elements/input.py
@@ -2,7 +2,7 @@ from playwright.sync_api import expect, Locator
 import allure
 from elements.base_element import BaseElement
 from tools.logger import get_logger
-
+from ui_coverage_tool import ActionType
 logger = get_logger('INPUT')
 
 class Input(BaseElement):
@@ -14,6 +14,9 @@ class Input(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_row_locator(self, nth: int = 0, **kwargs) -> str:
+        return f"{super().get_row_locator(nth, **kwargs)}//input"
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" with value {value}'
 
@@ -22,6 +25,8 @@ class Input(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has value {value}'
 
@@ -29,3 +34,5 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
